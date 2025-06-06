451f9740391671c8b2720a1733038507b832964c
sellingqi <studentofe@gmail.com>
2025-06-06 21:46:46 +0900

    Update vocab.js

--- api/vocab.js
+++ api/vocab.js
@@ -122,7 +122,7 @@ async function generateVocabProblem(passage) {
     numberMap.push({ word, number: index }); // 정답 계산용
   }
 
-  const question = '다음 글의 밑줄 친 부분 중, 문맥상 낱말의 쓰임이 적절하지 않은 것은?\n${numberedPassage}';
+  const question = `다음 글의 밑줄 친 부분 중, 문맥상 낱말의 쓰임이 적절하지 않은 것은?\n${numberedPassage}`;
   const answerEntry = numberMap.find(entry => entry.word === replacement);
   const answer = answerEntry ? answerEntry.number : null;
 
